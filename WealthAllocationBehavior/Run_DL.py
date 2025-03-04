6eb7da4af34be62ca021e3a606af19de17d53d44
Jarl Vesterholm Jensen <JVJ@nationalbanken.dk>
2025-03-04 16:30:48 +0100

    allocation version

---
+++ WealthAllocationBehavior/Run_DL.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 09:25:53 2025
+
+@author: JVJ
+"""
+
+#%load_ext autoreload
+#%autoreload 2
+
+from rl_project.WealthBehavior import IncomeChannelsModel
+import numpy as np
+import sympy
+import matplotlib.pyplot as plt
+plt.rcParams.update({
+    "axes.grid": True,
+    "grid.color": "black",
+    "grid.alpha": 0.25,
+    "grid.linestyle": "--",
+    "font.size": 14                    # Set font size
+})
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+from rl_project.EconDLSolvers import choose_gpu
+
+# Hyperparameters
+device = choose_gpu()
+model_DL = {}
+
+
+K_time = 3.0
+model_DL['DeepVPD'] = IncomeChannelsModel.WealthBehaviorModelClass(algoname='DeepVPD', device=device,train={'K_time': K_time})
+model_DL['DeepVPD'].solve(do_print=True)
